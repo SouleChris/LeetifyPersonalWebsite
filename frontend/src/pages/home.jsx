1f510abc8d267d841acb7027e24a66288e274d76
Christopher Soule <chrissoule02@gmail.com>
2026-02-26 09:15:08 -0500

    fixed page formatting on watches and clothing. Updated styling

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,22 @@
 export default function Home() {
   return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1></h1>
-      <p></p>
-      <p></p>
+    <body>
+
+  <header class="navbar">
+
+  </header>
+  <section class="content-grid">
+
+    <div class="col-left">
+      <h2>Welcome to a new kind of agency</h2>
     </div>
+
+    <div class="col-right">
+      <p>Description text here</p>
+    </div>
+
+  </section>
+
+</body>
   )
 }
